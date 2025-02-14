cfa4fade3776124a9a72df70e20c90af6185d792
Santigie Sankoh <>
2025-02-14 14:43:48 +0200

    Successful commit for the Regex - Onboarding Hackathon

--- main.py
+++ main.py
@@ -2,44 +2,44 @@ import re
 
 
 # 1 Email Addresses
-def extract_email(test):
-    email_regex = r"t.kuir@alustudent.com"
-    return re.findall(email_regex, test)
+def extract_email(outcome):
+    email_regex = r"m.ahol@alustudent.com"
+    return re.findall(email_regex, outcome)
 
 
 # 2 URLS
-def extract_url(test):
-    url_regex = r"https://www.equitechfutures.com/"
-    return re.findall(url_regex, test)
+def extract_url(outcome):
+    url_regex = r"https://www.alueducation.com/"
+    return re.findall(url_regex, outcome)
 
 
 # 3 Phone Numbers
-def extract_phone_numbers(test):
-    phone_numbers_regex = r"0794415424"
-    return re.findall(phone_numbers_regex, test)
+def extract_phone_numbers(outcome):
+    phone_numbers_regex = r"+250794423393"
+    return re.findall(phone_numbers_regex, outcome)
 
 
 # 4 Credit Card
-def extract_credit_card(test):
-    credit_card_regex = r"453-354-123-456"
-    return re.findall(credit_card_regex, test)
+def extract_credit_card(outcome):
+    credit_card_regex = r"879-428-479-092"
+    return re.findall(credit_card_regex, outcome)
 
 
 # Test the Functions
 def test_regex_patterns():
-    # Test Strings
+    # Outcome Strings
 
-    test_email = "t.kuir@alustudent.com"
-    test_url = "https://www.equitechfutures.com/"
-    test_phone_number = "0794415424"
-    test_credit_card = "453-354-123-456"
+    test_email = "m.ahol@alustudent.com"
+    test_url = "https://www.alueducation.com/"
+    test_phone_number = "+250794423393"
+    test_credit_card = "879-428-479-092"
 
-    # Extract Data
+    # Extract Outcome
 
-    print(f"Aluel extract email: \n{test_email}\n")
-    print(f"Equitech Futures Site: \n{test_url}\n")
-    print(f"Aluel Tabby's phone number: \n{test_phone_number}\n")
-    print(f"My personal credit card: \n{test_credit_card}\n")
+    print(f"Monica Akoi's relevant email: \n{test_email}\n")
+    print(f"African Leadership University's Site: \n{test_url}\n")
+    print(f"Extracted Phone number: \n{test_phone_number}\n")
+    print(f"Credit Card: \n{test_credit_card}\n")
 
 
 if __name__ == "__main__":
